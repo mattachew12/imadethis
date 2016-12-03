47f1dc12fd3502ffa5cd9255cd519af5c38851f7
Tanuj Sane <tsane@wpi.edu>
2016-12-03 15:04:32 -0500

    Restructuring

---
+++ public/client.js
@@ -0,0 +1,12 @@
+/* 
+ * client.js
+ * Function definitions for client-side code
+ *
+ * Authors:
+ * - Tanuj Sane
+ * - Nikhil Castelino
+ * - Matthew Piazza
+ *
+ * Usage:
+ * <script src="client.js"></script>
+ */
