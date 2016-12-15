1adf60655529eb530e1593794f3745e3b7de3df7
Tanuj Sane <tsane@wpi.edu>
2016-12-15 00:53:24 -0500

    Adding nav functionality

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -11,10 +11,16 @@
  * <script src="navbar.js"></script>
  */
 
-function performLogout() {            
+var account = JSON.parse(window.localStorage.getItem('account'));
+
+function performLogout() {
     document.cookie = 'user=; expires=Thu, 01-Jan-1970 00:00:01 GMT;';
     document.cookie = 'password=; expires=Thu, 01-Jan-1970 00:00:01 GMT;';
     window.sessionStorage.removeItem('account');
     window.localStorage.removeItem('account');
     window.location = 'login.html';
-} 
+}
+
+function navClick(id) {
+
+}
